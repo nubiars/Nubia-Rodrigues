744bf2c5de9feb26a745e7997742c9ba9fd621d7
Núbia <92464703+nubiars@users.noreply.github.com>
2021-12-23 15:50:34 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,27 +15,16 @@ for (const link of links) {
   })
 }
 
-const sections = document.querySelectorAll('main section[id]')
+const backToTopButton = document.querySelector('.back-to-top')
 
-function activateMenuAtCurrentSection() {
-  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
-
-  for (const section of sections) {
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionId = section.getAttribute('id')
-
-    const checkpointStart = checkpoint >= sectionTop
-    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
-
-    if (checkpointStart && checkpointEnd) {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.add('active')
-    } else {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.remove('active')
-    }
+function backToTop() {
+  if (window.scrollY >= 300) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
+
+window.addEventListener('scroll', function () {
+  backToTop()
+})
